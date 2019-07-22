00658c9d390f64f59efde77213ab28ade415827a
Menno Veerman <mveerman@test3.irc.sara.nl.irc.sara.nl>
2019-07-22 13:11:17 +0200

    error fix in CMakeLists Add trace gases to rcemip input (better comparison with RRTMGP)

--- rcemip/test_rcemip_input.py
+++ rcemip/test_rcemip_input.py
@@ -7,7 +7,7 @@ nc_file = nc.Dataset("test_rcemip_input.nc", mode="w", datamodel="NETCDF4", clob
 
 # Radiation profiles.
 z_top = 100.e3
-dz = 500.
+dz = 5.
 z  = np.arange(dz/2, z_top, dz)
 zh = np.arange(   0, z_top-dz/2, dz)
 zh = np.append(zh, z_top)
@@ -50,18 +50,26 @@ def calc_p_q_T(z):
 
 p_lay,   h2o, T_lay = calc_p_q_T( z)
 p_lev, dummy, T_lev = calc_p_q_T(zh)
-
-co2 =  348.e-6
-ch4 = 1650.e-9
-n2o =  306.e-9
-n2 = 0.7808
-o2 = 0.2095
+print("nlev",p_lay.shape)
+print(np.argmin(np.where(p_lay<9948.4,0,1)))
+co2 =  400.e-6
+ch4 = 1831.47094727e-9
+n2o =  326.98800659e-9
+n2 = 0.78100002
+o2 = 0.20900001
+cfc11 = 233.0798645e-12
+cfc12 = 520.58099365
+hcfc22=229.542e-12
+co =  1.19999e-7
+cf4 = 81.09249115e-12
+hfc143 = 15.25278091e-12
+hfc134 = 80.51573181e-12
 
 g1 = 3.6478
 g2 = 0.83209
 g3 = 11.3515
 p_hpa = p_lay/100.
-o3 = g1 * p_hpa**g2 * np.exp(-p_hpa/g3) * 1e-6
+o3 = np.minimum(1.4e-5,np.maximum(8.6e-9,g1 * p_hpa**g2 * np.exp(-p_hpa/g3) * 1e-6))
 
 nc_group_rad = nc_file.createGroup("radiation")
 
@@ -90,6 +98,13 @@ nc_O3  = nc_group_rad.createVariable("o3" , float_type, ("lay"))
 nc_H2O = nc_group_rad.createVariable("h2o", float_type, ("lay"))
 nc_N2  = nc_group_rad.createVariable("n2" , float_type, ("lay"))
 nc_O2  = nc_group_rad.createVariable("o2" , float_type, ("lay"))
+nc_X1  = nc_group_rad.createVariable("cfc11" , float_type, ("lay"))
+nc_X2  = nc_group_rad.createVariable("cfc12" , float_type, ("lay"))
+nc_X3  = nc_group_rad.createVariable("hcfc22" , float_type, ("lay"))
+nc_X4  = nc_group_rad.createVariable("co" , float_type, ("lay"))
+nc_X5  = nc_group_rad.createVariable("cf4" , float_type, ("lay"))
+nc_X5  = nc_group_rad.createVariable("hfc143a" , float_type, ("lay"))
+nc_X6  = nc_group_rad.createVariable("hfc134a" , float_type, ("lay"))
 
 nc_CO2[:] = co2
 nc_CH4[:] = ch4
@@ -98,5 +113,14 @@ nc_O3 [:] = o3 [:]
 nc_H2O[:] = h2o[:]
 nc_N2 [:] = n2
 nc_O2 [:] = o2
+nc_X1 [:] = cfc11
+nc_X2 [:] = cfc12
+nc_X3 [:] = hcfc22
+nc_X4 [:] = co
+nc_X4 [:] = cf4
+nc_X5 [:] = hfc143
+nc_X6 [:] = hfc134
 
+
+print(np.max(h2o))
 nc_file.close()
